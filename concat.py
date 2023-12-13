db0772053436237ca0a366efc7caf3dab7b0f33d
ugsakshii20 <138098129+ugsakshii20@users.noreply.github.com>
2023-12-13 08:43:43 +0530

    Create concat.py

---
+++ concat.py
@@ -0,0 +1,9 @@
+# Input: Get two strings from the user
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+
+# Concatenate the two strings
+concatenated_string = string1 + " " + string2
+
+# Display the result
+print("Concatenated String:", concatenated_string)
