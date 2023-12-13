ea9618f5c124cc9034d5688af6e86f7fb57d87be
ugsakshii20 <138098129+ugsakshii20@users.noreply.github.com>
2023-12-13 10:11:09 +0530

    Create greatestno.py

---
+++ greatestno.py
@@ -0,0 +1,8 @@
+# Get user input
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+# Find greatest number
+greatest = max(num1, num2, num3)
+print(f"The greatest number is: {greatest}")
